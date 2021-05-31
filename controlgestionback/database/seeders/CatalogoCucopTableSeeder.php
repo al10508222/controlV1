d7f47bf04398e0cef07fcc2b7c9b250f7f806646
gamoma@live.com.mx <gamoma@live.com.mx>
2021-05-30 22:47:46 -0500

    cambios

--- controlgestionback/database/seeders/CatalogoCucopTableSeeder.php
+++ controlgestionback/database/seeders/CatalogoCucopTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 
-class CatalogoCucopTableSeeder extends Seeder
+class CatCucopTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
