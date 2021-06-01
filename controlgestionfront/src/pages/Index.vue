13a8f8d7a799d2f888b4b3d12b4ff28cea957d6a
gamoma@live.com.mx <gamoma@live.com.mx>
2021-06-01 16:06:38 -0700

    Cambios de Pedro

--- controlgestionfront/src/pages/Index.vue
+++ controlgestionfront/src/pages/Index.vue
@@ -27,7 +27,7 @@ export default {
         { title: 'Puestos', path: '/positions', permission: 'positions-view', description: 'Administra los puestos de trabajo disponibles en la institución' },
         { title: 'Dashboard', path: '/dashboard', permission: 'dashboard-view', description: 'Vista general del sistema' },
         { title: 'Calendario', path: '/calendar', permission: 'calendar-view', description: 'Accede a un calendario para planear eventos o programar actividades' },
-        { title: 'Cucop', path: '/cucop', permission: 'cucop-view', description: 'Catálogo CuCops' },
+        { title: 'Catálogo CuCop', path: '/cucop', permission: 'cucop-view', description: 'Detalle del Catálogo' },
         // { title: 'Proyectos', path: '/psp/projects' },
         // { title: 'Proveedores', path: '/psp/providers' },
         // { title: 'Memos de Contrato', path: '/psp/hiringMemos' },
